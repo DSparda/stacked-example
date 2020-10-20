99f73ff17a1f1eb97a9ae1f9651e3d8539cc3ffc
Nguyễn Đức Hưng <dhungst1@gmail.com>
2020-10-20 08:11:16 +0700

    Add reactive feature for SingleIncreaseCounterView when tapping to double counter

--- lib/ui/smart_widgets/single_increase_counter/single_increase_counter_viewmodel.dart
+++ lib/ui/smart_widgets/single_increase_counter/single_increase_counter_viewmodel.dart
@@ -2,7 +2,7 @@ import 'package:my_app/app/locator.dart';
 import 'package:my_app/services/counter_service.dart';
 import 'package:stacked/stacked.dart';
 
-class SingleIncreaseCounterViewModel extends BaseViewModel {
+class SingleIncreaseCounterViewModel extends ReactiveViewModel {
   final _counterService = locator<CounterService>();
   int get counter => _counterService.counter;
 
@@ -10,4 +10,7 @@ class SingleIncreaseCounterViewModel extends BaseViewModel {
     _counterService.incrementCounter();
     notifyListeners();
   }
+
+  @override
+  List<ReactiveServiceMixin> get reactiveServices => [_counterService];
 }
